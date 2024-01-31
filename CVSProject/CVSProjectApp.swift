9fa49f739fcdd0f3a6fb5c1fe3ee8916debe9ddb
jael <jael.ruvalcaba@nmi.com>
2024-01-31 06:52:43 -0800

    Initial Commit

---
+++ CVSProject/CVSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSProjectApp.swift
+//  CVSProject
+//
+//  Created by Jael Ruvalcaba on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
